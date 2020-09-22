4c67e78295d192deb716637205365a9960a47a7d
Zayan Syed <15SyedZ@wg6.co.uk>
2020-09-22 14:15:39 +0100

    Random

--- Data_Structures/Program.cs
+++ Data_Structures/Program.cs
@@ -4,6 +4,10 @@ namespace Data_Structures
 {
     class Program
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
