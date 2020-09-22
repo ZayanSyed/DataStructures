6db974d6c107420177f258e006c84f36884f2d37
Zayan Syed <15SyedZ@wg6.co.uk>
2020-09-22 15:15:15 +0100

    Stack part 1

--- Data_Structures/Program.cs
+++ Data_Structures/Program.cs
@@ -4,13 +4,17 @@ namespace Data_Structures
 {
     class Program
     {
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="args"></param>
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stack a = new Stack(10);
+            a.Pop();
+            try
+            {
+                a.Pop();
+
+            }
+            catch { }
         }
     }
 }
---
+++ Data_Structures/Stack.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Structures
+{
+    class Stack
+    {
+        int[] stack;
+        int sp;
+        int max;
+
+        public Stack(int size)
+        {
+            max = size;
+            stack = new int[max];
+            sp = 0;
+        }
+
+        public void Push(int item)
+        {
+            if (sp < max)
+            {
+                stack[sp] = item;
+                sp++;
+            }
+            throw new Exception("stack full");
+        }   
+
+        public int Pop()
+        {
+            if (sp > 0)
+            {
+                sp--;
+                return stack[sp];
+            }
+            else
+            {
+                throw new Exception("Stack empty");
+            }
+        }
+
+  
+    }
+}
